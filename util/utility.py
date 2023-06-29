18753c836ba95ec76cedbe0e7366fb722bdeeb57
wdhudiekou <wangdi_1211@njust.edu.cn>
2023-06-29 10:38:08 +0800

    Create utility.py

---
+++ util/utility.py
@@ -0,0 +1,111 @@
+import torch
+
+def rgb2ycbcr_pt(img, y_only=False):
+    """Convert RGB images to YCbCr images (PyTorch version).
+    It implements the ITU-R BT.601 conversion for standard-definition television. See more details in
+    https://en.wikipedia.org/wiki/YCbCr#ITU-R_BT.601_conversion.
+    Args:
+        img (Tensor): Images with shape (n, 3, h, w), the range [0, 1], float, RGB format.
+         y_only (bool): Whether to only return Y channel. Default: False.
+    Returns:
+        (Tensor): converted images with the shape (n, 3/1, h, w), the range [0, 1], float.
+    """
+    if y_only:
+        weight = torch.tensor([[65.481], [128.553], [24.966]]).to(img)
+        out_img = torch.matmul(img.permute(0, 2, 3, 1), weight).permute(0, 3, 1, 2) + 16.0
+    else:
+        weight = torch.tensor([[65.481, -37.797, 112.0], [128.553, -74.203, -93.786], [24.966, 112.0, -18.214]]).to(img)
+        bias = torch.tensor([16, 128, 128]).view(1, 3, 1, 1).to(img)
+        out_img = torch.matmul(img.permute(0, 2, 3, 1), weight).permute(0, 3, 1, 2) + bias
+
+    out_img = out_img / 255.
+    return out_img
+
+def RGB2YCrCb(input_im):
+    im_flat = input_im.transpose(1, 3).transpose(
+        1, 2).reshape(-1, 3)  # (nhw,c)
+    R = im_flat[:, 0]
+    G = im_flat[:, 1]
+    B = im_flat[:, 2]
+    Y = 0.299 * R + 0.587 * G + 0.114 * B
+    Cr = (R - Y) * 0.713 + 0.5
+    Cb = (B - Y) * 0.564 + 0.5
+    Y = torch.unsqueeze(Y, 1)
+    Cr = torch.unsqueeze(Cr, 1)
+    Cb = torch.unsqueeze(Cb, 1)
+    temp = torch.cat((Y, Cr, Cb), dim=1).cuda()
+    out = (
+        temp.reshape(
+            list(input_im.size())[0],
+            list(input_im.size())[2],
+            list(input_im.size())[3],
+            3,
+        )
+        .transpose(1, 3)
+        .transpose(2, 3)
+    )
+    return out
+
+def YCrCb2RGB(input_im):
+    im_flat = input_im.transpose(1, 3).transpose(1, 2).reshape(-1, 3)
+    mat = torch.tensor(
+        [[1.0, 1.0, 1.0], [1.403, -0.714, 0.0], [0.0, -0.344, 1.773]]
+    ).cuda()
+    bias = torch.tensor([0.0 / 255, -0.5, -0.5]).cuda()
+    temp = (im_flat + bias).mm(mat).cuda()
+    out = (
+        temp.reshape(
+            list(input_im.size())[0],
+            list(input_im.size())[2],
+            list(input_im.size())[3],
+            3,
+        )
+        .transpose(1, 3)
+        .transpose(2, 3)
+    )
+    return out
+
+
+def rgb2ycbcr(img: torch.Tensor):
+    """
+    Converts a batch of RGB images to YCbCr.
+
+    :param img:
+        a batch of RGB image tensors.
+    :return:
+        a batch of YCbCr images.
+    """
+
+    if len(img.shape) != 4:
+        raise ValueError('Input images must have four dimensions, not %d' % len(img.shape))
+
+    Y = 0. + .299 * img[:, 0] + .587 * img[:, 1] + .114 * img[:, 2]
+    Cb = 128. - .169 * img[:, 0] - .331 * img[:, 1] + .5 * img[:, 2]
+    Cr = 128. + .5 * img[:, 0] - .419 * img[:, 1] - .081 * img[:, 2]
+    return torch.cat((Y.unsqueeze(1), Cb.unsqueeze(1), Cr.unsqueeze(1)), 1)
+
+
+def ycbcr2rgb(img: torch.Tensor):
+    """
+    Converts a batch of YCbCr images to RGB.
+
+    :param img:
+        a batch of YCbCr image tensors.
+    :return:
+        a batch of RGB images.
+    """
+
+    if len(img.shape) != 4:
+        raise ValueError('Input images must have four dimensions, not %d' % len(img.shape))
+
+    R = img[:, 0] + 1.4 * (img[:, 2] - 128.)
+    G = img[:, 0] - .343 * (img[:, 1] - 128.) - .711 * (img[:, 2] - 128.)
+    B = img[:, 0] + 1.765 * (img[:, 1] - 128.)
+    return torch.cat((R.unsqueeze(1), G.unsqueeze(1), B.unsqueeze(1)), 1)
+
+
+def clip_gradient(optimizer, grad_clip):
+    for group in optimizer.param_groups:
+        for param in group['params']:
+            if param.grad is not None:
+                param.grad.data.clamp_(-grad_clip, grad_clip)
